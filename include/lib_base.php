cfda2f5293b80dedef631523f6ca97d474a0a92e
Xingzhang Lin <1129507496@qq.com>
2016-10-29 23:06:42 +0800

    wuxianjifenlei

---
+++ include/lib_base.php
@@ -0,0 +1,34 @@
+<?php
+
+	/**
+	 * 系统基础函数
+	 */
+
+	 
+	/**
+	 * 递归转义数组
+	 * @params {Array} $arr 转义的数组
+	 * @return {Array} $arr 转义之后的数组
+	 */
+	function _addslashes( $arr ) {
+		
+		foreach( $arr as $k => $v ) {
+			
+			// 判断是否数组
+			if ( is_array($v) ) {
+				$arr[$k] = _addslashes($v);
+			}
+			
+			// 判断是否是字符串
+			if ( is_string($v) ) {
+				$arr[$k] = addslashes($v);
+			}
+			
+		}
+		
+		return $arr;
+		
+	} 
+
+	
+?>
--- index.php
+++ index.php
@@ -7,25 +7,7 @@
 	 */
 
 	require('./include/init.php');
-	
-	Log::write('基础');
-	
-	class Mysql {
-		public function query ( $sql ) {
-			// 查询
-			// 记录
-			Log::write($sql);
-		}
-	}
 
-	$mysql = new Mysql();
+	print_r($_GET);	
 	
-	for ( $i=0; $i<1000; $i++ ) {
-		$sql = "select goods_id, goods_name, shop_price from goods, select goods_id, goods_name, shop_price from goods,select goods_id, goods_name, shop_price from goods,select goods_id, goods_name, shop_price from goods,select goods_id, goods_name, shop_price from goods,select goods_id, goods_name, shop_price from goods,select goods_id, goods_name, shop_price from goods,select goods_id, goods_name, shop_price from goods,select goods_id, goods_name, shop_price from goods,select goods_id, goods_name, shop_price from goods,select goods_id, goods_name, shop_price from goods,select goods_id, goods_name, shop_price from goods,select goods_id, goods_name, shop_price from goods where goods_id=" . mt_rand(1, 1000);
-		$mysql->query($sql);
-		usleep(10000);
-	}
 	
-	echo '执行完成';
-
-?>
